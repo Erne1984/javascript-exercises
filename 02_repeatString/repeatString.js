b0ff1c37aae28f40bc820b85885226f7010c4cd2
ernesto <ernestofamorim@gmail.com>
2023-12-20 09:12:15 -0300

    exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let newString = string;
+
+    if(num === 0) return newString = "";
+     
+    if(num <= 0) return newString = "ERROR";
+    
+    for(let i = 1; i < num; i++){
+        newString += string;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
