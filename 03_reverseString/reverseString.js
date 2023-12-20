777282ad6115469ec85088a13f7ac7764e07e999
ernesto <ernestofamorim@gmail.com>
2023-12-20 15:24:27 -0300

    exercise 4 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if(string.split(" ") > 1){
+        let newConcatString = "";
 
+        for(let i = 0; i < string.length; i ++){
+
+            newConcatString += string[i].split("").reverse().join("") + " ";  
+
+            console.log(i)
+        }
+
+        return newConcatString;
+
+    } else{
+        let reversedString = string.split("").reverse().join("");
+
+        return reversedString;
+    }
 };
 
 // Do not edit below this line
