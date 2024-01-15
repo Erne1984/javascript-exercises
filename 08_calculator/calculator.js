b1ff588a9824785b0c657a38c97bf33a8bfb7608
ernesto <ernestofamorim@gmail.com>
2024-01-15 09:34:42 -0300

    Exercise 08, 09, 10 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,61 @@
-const add = function() {
+const add = function(value1, value2) {
 	
+  return value1 + value2;
 };
 
-const subtract = function() {
+const subtract = function(value1, value2) {
+	return value1 - value2;
+};
+
+const sum = function(array) {
+  if(array.length === 0) return 0;
+  if(array.length === 1) return array[0];
+
+  let total = 0;
+
+  array.forEach(value => {
+    total += value;
+  });
+
+  return total;
+
+};
+
+const multiply = function(array) {
+  let result = 1;
+
+  array.forEach(value => {
+    result *= value;
+  });
+    
+  return result;
+};
+
+const power = function(value, power) {
+  let result = 1;
 	
+  for(let i = 0; i < power; i++){
+    result *= value;
+  }
+
+  return result;
 };
 
-const sum = function() {
-	
+const factorial = function(value) {
+
+  if(value === 0) return 1;
+
+  if(value === 1) return 1;
+
+  let result = 1;
+
+  for(let i = value; i > 0; i--){
+    result *= i;  
+  }
+
+  return result;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(sequence) {
 
+    if(typeof sequence == "string") parseFloat(sequence);
+    if(sequence == 0) return 0;
+    if(sequence < 0) return "OOPS";
+    
+
+    let firstNumber = 1;
+    let secondNumber = 0;
+    
+    for(let i = 1; i < sequence; i++){
+
+        let current = firstNumber + secondNumber;
+
+        console.log(current);
+
+        secondNumber = firstNumber;
+
+        firstNumber = current;
+
+    }
+
+    return firstNumber;
 };
+
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
