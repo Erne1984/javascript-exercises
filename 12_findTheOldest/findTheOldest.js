efeb0ac9056a08575eba076167f5e4b479fedc11
ernesto <ernestofamorim@gmail.com>
2024-01-16 01:22:06 -0300

    exercise 11 and 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (listPersons) {
+    let oldestAge = 0;
+    let age = 0;
+    let oldestPerson = {};
 
+    listPersons.forEach(person => {
+        age = person.yearOfDeath - person.yearOfBirth;
+
+        if (typeof person.yearOfDeath === "undefined") {
+            age = new Date().getFullYear() - person.yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = person;
+            }
+        }
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+
+    });
+
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
